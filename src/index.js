e8467a4d3ecee3f8a18560f2bb7eab1b58c61631
otakyoon <youenn.thirion@gmail.com>
2024-05-01 11:50:30 +0200

    3.3.h.affichage du player

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '../css/style.scss';
 import Space from '../assets/images/space.png';
 
 import InvaderController from './InvaderController';
+import Player from './player';
 
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
@@ -12,10 +13,12 @@ background.src = Space;
 ctx.Image = background;
 
 const invaderController = new InvaderController(canvas);
+const player = new Player(canvas,3);
 
 function game(){
     ctx.drawImage(background, 0,0,canvas.width, canvas.height);
     invaderController.draw(ctx);
+    player.draw(ctx);
 }
 
 setInterval(game,1000/60);
---
+++ src/player.js
@@ -0,0 +1,77 @@
+import PlayerImg from '../assets/images/player.png';
+
+export default class Player {
+ //   rightPressed = false;
+   // leftPressed = false;
+    //shootPressed = false;
+
+    constructor(canvas, velocity) {
+        this.canvas = canvas;
+        this.velocity = velocity;
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height - 75;
+        this.width = 50;
+        this.height = 48;
+        this.image = new Image();
+        this.image.src = PlayerImg;
+        //this.bulletController = bulletController;
+
+       // document.addEventListener("keydown", this.keydown);
+        //document.addEventListener("keyup", this.keyup);
+    }
+
+    draw(ctx) {
+       // this.move();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        //this.collideWithWalls();
+        /*if (this.shootPressed) {
+            console.log("shoot");
+            this.bulletController.shoot(this.x + this.width / 2, this.y, 4,10);
+        }*/
+
+    }
+
+    move() {
+        if (this.rightPressed) {
+            this.x += this.velocity;
+        } else if (this.leftPressed) {
+            this.x += -this.velocity;
+        }
+    }
+
+    collideWithWalls() {
+        //left
+        if (this.x < 0) {
+            this.x = 0;
+        }
+
+        //right
+        if (this.x > this.canvas.width - this.width) {
+            this.x = this.canvas.width - this.width;
+        }
+    }
+
+    keydown = (event) => {
+        if (event.code === "ArrowRight") {
+            this.rightPressed = true;
+        }
+        if (event.code === "ArrowLeft") {
+            this.leftPressed = true;
+        }
+        if (event.code === "Space") {
+            this.shootPressed = true;
+        }
+    };
+
+    keyup = (event) => {
+        if (event.code == "ArrowRight") {
+            this.rightPressed = false;
+        }
+        if (event.code == "ArrowLeft") {
+            this.leftPressed = false;
+        }
+        if (event.code == "Space") {
+            this.shootPressed = false;
+        }
+    };
+}
