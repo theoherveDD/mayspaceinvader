7be59e132227ab6ee61284285cda7abea4da91a3
otakyoon <youenn.thirion@gmail.com>
2024-05-01 14:50:41 +0200

    3.3.j.1 collision bullet player

--- src/index.js
+++ src/index.js
@@ -8,25 +8,39 @@ import Bulletcontroller from './BulletController';
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 canvas.height = 600;
-canvas.width= 600;
+canvas.width = 600;
 const background = new Image();
 background.src = Space;
 ctx.Image = background;
 
-const playerBulletController = new Bulletcontroller(canvas,10, "red", true);
-const player = new Player(canvas,3,playerBulletController);
+const playerBulletController = new Bulletcontroller(canvas, 10, "red", true);
+const player = new Player(canvas, 3, playerBulletController);
 
-const invadersBulletController = new Bulletcontroller(canvas,4, "white", false);
+const invadersBulletController = new Bulletcontroller(canvas, 4, "white", false);
 const invaderController = new InvaderController(canvas, invadersBulletController, playerBulletController);
 
+let isGameOver = false;
 
+function game() {
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-function game(){
-    ctx.drawImage(background, 0,0,canvas.width, canvas.height);
-    invaderController.draw(ctx);
-    player.draw(ctx);
-    playerBulletController.draw(ctx);
-    invadersBulletController.draw(ctx)
+    if (!isGameOver) {
+        invaderController.draw(ctx);
+        player.draw(ctx);
+        playerBulletController.draw(ctx);
+        invadersBulletController.draw(ctx);
+    }
+
+    checkGameOver();
 }
 
-setInterval(game,1000/60);
+setInterval(game, 1000 / 60);
+
+function checkGameOver(){
+    if(isGameOver){
+        return;
+    }
+    if(invadersBulletController.collideWith(player)){
+        isGameOver=true;
+    }
+}
