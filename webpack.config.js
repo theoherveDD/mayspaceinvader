604b774776c73beaa7baf2f4764bfe24da5dc575
otakyoon <youenn.thirion@gmail.com>
2024-05-01 10:48:56 +0200

    3.3.f.affichage des invaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 module.exports = {
-    // 1
-    entry: './src/index.js',
-    // 2
-    output: {
-      path: '/dist',
-      filename: 'bundle.js'
-    },
-    // 3
-    devServer: {
-      static: './dist'
-    }
-  };
+  // 1
+  entry: './src/index.js',
+  // 2
+  output: {
+    path: '/dist',
+    filename: 'bundle.js'
+  },
+  // 3
+  devServer: {
+    static: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  },
+};
