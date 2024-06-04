b9bb37b2efacf3d8eaab95ae3ca5744f59e9c6ba
colin-lallauret <colinlallauret1@gmail.com>
2024-06-04 16:11:29 +0200

    Fix score problem

--- src/score.js
+++ src/score.js
@@ -2,7 +2,12 @@ let displayScore = document.getElementById("score");
 let score = 0;
 
 export function addScore(type) {
-    console.log(type);
+    console.log(score);
     score = ((score) + (type * 5));
+    displayScore.innerHTML = score;
+}
+
+export function resetScore() {
+    score = 0;
     displayScore.innerHTML = score;
 }
